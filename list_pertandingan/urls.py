b994e40986d9dd905e497371df9f157fe4e38396
Qistina Muharrifa <qistinamuharrifa@qistina.local>
2023-05-07 20:55:41 +0700

    menyelesaikan list pertandingan

--- list_pertandingan/urls.py
+++ list_pertandingan/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from list_pertandingan.views import *
 
-app_name = 'ist_pertandingan'
+app_name = 'list_pertandingan'
 
 urlpatterns = [
     path('', list_pertandingan, name='list_pertandingan'),
+    path('penonton/', list_pertandingan_penonton, name='list_pertandingan_penonton'),
+    path('manajer/', list_pertandingan_manajer, name='list_pertandingan_manajer'),
 ]
