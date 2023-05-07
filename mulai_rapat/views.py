a21e5e5ba2aa7e6104904d8823cd2b934bd9edfd
Assajjade <abdillah.assajjad@ui.ac.id>
2023-05-07 22:27:39 +0700

    Abdi : Nyoba push untuk 3 Modul

--- mulai_rapat/views.py
+++ mulai_rapat/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def mulai_rapat(request):
     return render(request, 'mulai_rapat.html')
+
+def rapat_pertandingan(request):
+    return render(request, 'rapat_pertandingan.html')
--- pembuatan_pertandingan/views.py
+++ pembuatan_pertandingan/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def pembuatan_pertandingan(request):
     return render(request, 'pembuatan_pertandingan.html')
+
+def pembuatan_pertandingan_lanjutan(request):
+    return render(request, 'pembuatan_pertandingan_lanjutan.html')
